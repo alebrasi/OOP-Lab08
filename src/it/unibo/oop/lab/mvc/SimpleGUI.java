622a701d309ba104356f0f48d41fb3ceb0b15c06
alebrasi <alessandro.brasini1@gmail.com>
2020-11-22 20:28:48 +0100

    Implemented main and SimpleGUI class

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,16 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -34,6 +41,52 @@ public final class SimpleGUI {
      * 
      */
 
+    public void addElementsToGUI() {
+        final JPanel canvas =  new JPanel();
+        final JPanel bottomPanel = new JPanel();
+
+        bottomPanel.setLayout(new BorderLayout());
+        canvas.setLayout(new BorderLayout());
+
+        final JTextField textField = new JTextField();
+        final JTextArea textArea = new JTextArea();
+        final JButton print = new JButton("Print");
+        final JButton showHistory = new JButton("Show history");
+
+        bottomPanel.add(showHistory, BorderLayout.EAST);
+        bottomPanel.add(print, BorderLayout.WEST);
+
+        canvas.add(textField, BorderLayout.NORTH);
+        canvas.add(textArea, BorderLayout.CENTER);
+
+        canvas.add(bottomPanel, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+
+        final Controller controller = new ControllerImpl();
+
+        print.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                controller.setNextString(textField.getText());
+                controller.printCurrentString();
+                textField.setText("");
+            }
+
+        });
+
+        showHistory.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                for (final String s : controller.getHistory()) {
+                    textArea.append(s + "\n");
+                }
+            }
+
+        });
+    }
+
     /**
      * builds a new {@link SimpleGUI}.
      */
@@ -60,6 +113,14 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        addElementsToGUI();
+    }
+
+    public static void main(final String... args) {
+        new SimpleGUI();
     }
 
 }
